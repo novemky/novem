1969371a069389141a77ac782fde7675d9b420fd
novemky <145815511+novemky@users.noreply.github.com>
2024-11-19 15:22:26 +0800

    config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'paurom');
+define('DB_PASSWORD', 'paurom');
 define('DB_NAME', 'demo');
  
 /* Attempt to connect to MySQL database */
